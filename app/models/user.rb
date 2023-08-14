ec7b230e6ff73ef68ddf232e37494eb6a87d60ba
eudondian <eudondian@gmail.com>
2023-08-14 15:30:13 +0100

    Modify user.rb to include authentication and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  #  :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, 
+         :recoverable, :rememberable, :validatable
   has_many :expenses, foreign_key: :author_id
   has_many :groups, foreign_key: :author_id
+
+  attr_accessor :full_name
+
+  validates :full_name, presence: true
 end

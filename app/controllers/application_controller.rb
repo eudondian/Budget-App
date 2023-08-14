edd6ff5bd5c1ecb5ca52106c82df483e8379cb2b
eudondian <eudondian@gmail.com>
2023-08-14 15:28:40 +0100

    Add methods for User authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
-end
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    # ... other code ...
+  
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:full_name])
+    end
+  end
+  

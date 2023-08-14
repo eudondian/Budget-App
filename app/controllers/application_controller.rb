b7a01dad07df985c80f24f61c1a880e7b92b7bea
eudondian <eudondian@gmail.com>
2023-08-14 17:10:58 +0100

    Fix rubocop errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
-  
-    # ... other code ...
-  
-    protected
-  
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:full_name])
-    end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # ... other code ...
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:full_name])
   end
-  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   #  :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, 
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :expenses, foreign_key: :author_id
   has_many :groups, foreign_key: :author_id

e68e4f6f5db500f74881308b5ff5de1b53462b37
eudondian <eudondian@gmail.com>
2023-08-16 20:44:21 +0100

    Modify routes file to include transaction/cotegories pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "splash#splash"
+  authenticated :user do
+    root to: "categories#index", as: :authenticated_root
+  end
+  resources :categories do
+    resources :expenses, only: [:index, :new, :create, :edit, :update, :destroy]
+  end
+  
+  # resources :categories, only: [:index, :new, :create, :edit, :update, :destroy] do
+  #   resources :expenses, only: [:index, :new, :create, :edit, :update, :destroy]
+  #   # resources :records, only: [:index, :new, :create, :edit, :update, :destroy]
+  
+
+  # Other routes...
 end

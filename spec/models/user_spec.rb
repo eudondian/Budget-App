a27e84c94cca4e1a42b880d65491e969d0fec185
eudondian <eudondian@gmail.com>
2023-08-16 22:18:51 +0100

    Add unit test for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,59 @@
 require 'rails_helper'
 
-RSpec.describe Users, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model do
+  it "is valid with valid attributes" do
+    user = User.new(
+      full_name: "John Doe",
+      email: "john@example.com",
+      password: "password123"
+    )
+    expect(user).to be_valid
+  end
+
+  it "is not valid without a full name" do
+    user = User.new(
+      email: "john@example.com",
+      password: "password123"
+    )
+    expect(user).not_to be_valid
+  end
+
+  it "is not valid with a short full name" do
+    user = User.new(
+      full_name: "John",
+      email: "john@example.com",
+      password: "password123"
+    )
+    expect(user).not_to be_valid
+  end
+
+  it "is not valid without an email" do
+    user = User.new(
+      full_name: "John Doe",
+      password: "password123"
+    )
+    expect(user).not_to be_valid
+  end
+
+  it "is not valid with a duplicate email" do
+    User.create(
+      full_name: "John Doe",
+      email: "john@example.com",
+      password: "password123"
+    )
+    user = User.new(
+      full_name: "Jane Doe",
+      email: "john@example.com",
+      password: "password456"
+    )
+    expect(user).not_to be_valid
+  end
+
+  it "is not valid without a password" do
+    user = User.new(
+      full_name: "John Doe",
+      email: "john@example.com"
+    )
+    expect(user).not_to be_valid
+  end
 end

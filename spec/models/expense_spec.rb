451933d2ef7d8cf7c9848ec6bbc7e09242af6eee
eudondian <eudondian@gmail.com>
2023-08-16 22:19:20 +0100

    Add unit test for expenses

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(full_name: "John Doe", email: "john@example.com", password: "password123") }
+  let(:category) { Category.create(name: "Groceries", icon: "grocery_icon", author: user) }
+
+  it "is valid with valid attributes" do
+    expense = Expense.new(
+      name: "Milk",
+      amount: 2.5,
+      category: category,
+      author: user
+    )
+    expect(expense).to be_valid
+  end
+
+  it "is not valid without a name" do
+    expense = Expense.new(
+      amount: 2.5,
+      category: category,
+      author: user
+    )
+    expect(expense).not_to be_valid
+  end
+
+  it "is not valid without an amount" do
+    expense = Expense.new(
+      name: "Milk",
+      category: category,
+      author: user
+    )
+    expect(expense).not_to be_valid
+  end
+
+  it "is not valid without a category" do
+    expense = Expense.new(
+      name: "Milk",
+      amount: 2.5,
+      author: user
+    )
+    expect(expense).not_to be_valid
+  end
 end

abd0a25338b375a81299bfc516f531e9097ad712
eudondian <eudondian@gmail.com>
2023-08-18 23:27:24 +0100

    Add test to user models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,58 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is valid with valid attributes" do
-    user = User.new(
-      full_name: "John Doe",
-      email: "john@example.com",
-      password: "password123"
-    )
+  it 'is valid with valid attributes' do
+    user = User.new(full_name: 'John Doe', email: 'john7@example.com', password: 'password123')
     expect(user).to be_valid
   end
 
-  it "is not valid without a full name" do
+  it 'is not valid without a full name' do
     user = User.new(
-      email: "john@example.com",
-      password: "password123"
+      email: 'john@example.com',
+      password: 'password123'
     )
     expect(user).not_to be_valid
   end
 
-  it "is not valid with a short full name" do
+  it 'is not valid with a short full name' do
     user = User.new(
-      full_name: "John",
-      email: "john@example.com",
-      password: "password123"
+      full_name: 'John',
+      email: 'john@example.com',
+      password: 'password123'
     )
     expect(user).not_to be_valid
   end
 
-  it "is not valid without an email" do
+  it 'is not valid without an email' do
     user = User.new(
-      full_name: "John Doe",
-      password: "password123"
+      full_name: 'John Doe',
+      password: 'password123'
     )
     expect(user).not_to be_valid
   end
 
-  it "is not valid with a duplicate email" do
+  it 'is not valid with a duplicate email' do
     User.create(
-      full_name: "John Doe",
-      email: "john@example.com",
-      password: "password123"
+      full_name: 'John Doe',
+      email: 'john@example.com',
+      password: 'password123'
     )
     user = User.new(
-      full_name: "Jane Doe",
-      email: "john@example.com",
-      password: "password456"
+      full_name: 'Jane Doe',
+      email: 'john@example.com',
+      password: 'password456'
     )
     expect(user).not_to be_valid
   end
 
-  it "is not valid without a password" do
+  it 'is not valid without a password' do
     user = User.new(
-      full_name: "John Doe",
-      email: "john@example.com"
+      full_name: 'John Doe',
+      email: 'john@example.com'
     )
     expect(user).not_to be_valid
   end
